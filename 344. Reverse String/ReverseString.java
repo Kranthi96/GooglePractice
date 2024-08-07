7ca760462d3d297b9dcad1e79ec3e05581cb5620
Kranthi Kumar <30343355+Kranthi96@users.noreply.github.com>
2024-08-07 23:21:44 +0530

    Create ReverseString.java

---
+++ 344. Reverse String/ReverseString.java
@@ -0,0 +1,14 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length-1;
+        while(left<=right)
+        { 
+            char t = s[left];
+            s[left] = s[right];
+            s[right] = t;
+            left++;
+            right--;
+        }
+        
+    }
+}
