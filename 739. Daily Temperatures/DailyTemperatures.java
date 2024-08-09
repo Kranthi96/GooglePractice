4e00fc4ce58e79acd0c9ad17dbe51f2c884db6d3
Kranthi Kumar <30343355+Kranthi96@users.noreply.github.com>
2024-08-09 23:36:56 +0530

    Update DailyTemperatures.java

--- 739. Daily Temperatures/DailyTemperatures.java
+++ 739. Daily Temperatures/DailyTemperatures.java
@@ -1,17 +1,36 @@
-class DailyTemperatures {
+class Solution {
     public int[] dailyTemperatures(int[] temps) {
-        int n= temps.length;
+        // int n= temps.length;
+        // int[] ans = new int[n];
+        // Deque<Integer> stack = new ArrayDeque<>();
+        // for(int i=0;i<n;i++){
+        //     int curr = temps[i];
+        //     while(!stack.isEmpty() && temps[stack.peek()]<curr ){
+        //         int prev = stack.pop();
+        //         ans[prev] = i - prev;
+        //     }
+        //     stack.push(i);
+        // }
+        // return ans;
+
+        int n = temps.length;
         int[] ans = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i=0;i<n;i++){
+        int max = 0;
+        for(int i=n-1; i>=0; i--){
             int curr = temps[i];
-            while(!stack.isEmpty() && temps[stack.peek()]<curr ){
-                int prev = stack.pop();
-                ans[prev] = i - prev;
+            if(max <= curr){
+                max = curr;
+                continue;
             }
-            stack.push(i);
+            int days = 1;
+            while(temps[i+days] <= curr){
+                days += ans[i+days];
+            }
+            ans[i]= days;
         }
         return ans;
+
         
     }
 }
+
