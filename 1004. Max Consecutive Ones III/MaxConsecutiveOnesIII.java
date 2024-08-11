3139ab960bf480859f33496039e7c72b631d8503
Kranthi Kumar <30343355+Kranthi96@users.noreply.github.com>
2024-08-11 22:06:26 +0530

    Create MaxConsecutiveOnesIII.java

---
+++ 1004. Max Consecutive Ones III/MaxConsecutiveOnesIII.java
@@ -0,0 +1,16 @@
+class MaxConsecutiveOnesIII {
+    public int longestOnes(int[] nums, int k) {
+       int left = 0, right;
+       for(right=0; right < nums.length; right++){
+        if(nums[right] == 0)
+            k--;
+        if(k<0){
+            if(nums[left++] ==0)
+                k++;
+        }
+            
+       }
+
+        return right-left;
+    }
+}
