494a5cc0711a832589a5ec5c2efe49cd6e7fe344
Kranthi Kumar <30343355+Kranthi96@users.noreply.github.com>
2026-01-02 07:51:38 +0530

    Create JumpGameII.java

---
+++ 45. Jump Game II/JumpGameII.java
@@ -0,0 +1,15 @@
+class JumpGameII {
+  public int jump(int[] nums) {
+      int jumps = 0;
+      int currentend = 0;
+      int farthest = 0;
+      for (int i = 0; i <= nums.length - 2; i++) {
+          farthest = Math.max(farthest, i + nums[i]);
+          if (i == currentend) {
+              jumps++;
+              currentend = farthest;
+          }
+      }
+      return jumps;
+  }
+}
